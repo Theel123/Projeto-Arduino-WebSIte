3ff3959b4155fac19d3d18d8200bb0b3ff9c1214
Matheus <theel.na@hotmail.com>
2019-11-09 20:37:46 -0300

    subindo

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt">
     <head>
+        <meta charset="UTF-8">
         <title>Oh Braia!</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="shortcut icon" href="imagens/braia.jpg">
